71d74bf61a540a7d5444128bb9c79b780c2b9f57
miglas <miglas@penguin>
2025-05-15 13:32:50 +0100

    type preservation adaptation for canonicals

--- theories/TypePreservationCanonical.v
+++ theories/TypePreservationCanonical.v
@@ -1,5 +1,5 @@
 Require Import List.
-Require Import LambdaM.
+Require Import Canonical.
 Require Import Autosubst.Autosubst.
 
 Import ListNotations.
@@ -12,21 +12,25 @@ Inductive type: Type := Base | Arr : type -> type -> type.
 (* Typing Rules *)
 (* ------------ *)
 
-Inductive sequent (Γ: var->type) : λm -> type -> Prop := 
+Inductive sequent (Γ: var->type) : λc -> type -> Prop := 
 | varAxiom (x: var) (A: type) :
-  Γ x = A -> sequent Γ (Var x) A
+  Γ x = A -> sequent Γ (Vari x) A
 
-| Right (t: λm) (A B: type) :
-  sequent (A .: Γ) t B -> sequent Γ (Lam t) (Arr A B)
+| Right (t: λc) (A B: type) :
+  sequent (A .: Γ) t B -> sequent Γ (Lamb t) (Arr A B)
+                                 
+| Left (x: var) (u: λc) (l: list λc) (A B C: type) :
+  Γ x = (Arr A B) -> sequent Γ u A -> list_sequent Γ B l C ->
+  sequent Γ (VariApp x u l) C
 
-| HeadCut (t u: λm) (l: list λm) (A B C: type) :
-  sequent Γ t (Arr A B) -> sequent Γ u A -> list_sequent Γ B l C ->
-  sequent Γ (mApp t u l) C
+| KeyCut (t: {bind λc}) (u: λc) (l: list λc) (A B C: type) :
+  sequent (A .: Γ) t B -> sequent Γ u A -> list_sequent Γ B l C ->
+  sequent Γ (LambApp t u l) C
 
-with list_sequent (Γ:var->type) : type -> (list λm) -> type -> Prop :=
+with list_sequent (Γ:var->type) : type -> (list λc) -> type -> Prop :=
 | nilAxiom (C: type) : list_sequent Γ C [] C
 
-| Lft (u: λm) (l: list λm) (A B C:type) :
+| Lft (u: λc) (l: list λc) (A B C:type) :
   sequent Γ u A -> list_sequent Γ B l C ->
   list_sequent Γ (Arr A B) (u :: l) C.
 
@@ -35,6 +39,28 @@ Hint Constructors sequent list_sequent : core.
 Scheme sim_sequent_ind := Induction for sequent Sort Prop
   with sim_list_sequent_ind := Induction for list_sequent Sort Prop.
 
+(* Append and @ admissability *)
+(* -------------------------- *)
+
+Lemma append_is_admissible Γ l l' A B C :
+  list_sequent Γ A l B -> list_sequent Γ B l' C ->
+  list_sequent Γ A (l++l') C.
+Proof.                  
+  intros H1 H2.
+  induction H1 ; asimpl ; auto.
+Qed.  
+
+Lemma app_is_admissible Γ t u l A B C :
+  sequent Γ t (Arr A B) -> sequent Γ u A -> list_sequent Γ B l C ->
+  sequent Γ t@(u,l) C.
+Proof.
+  destruct t ; intros ;
+    inversion H ; subst ;
+    econstructor ; eauto.
+  - eapply append_is_admissible ; eauto.
+  - eapply append_is_admissible ; eauto.
+Qed.
+    
 (* Subst and Ren Lemmas *)
 (* -------------------- *)
 
@@ -46,9 +72,9 @@ Lemma type_renaming Γ t A (s: sequent Γ t A) :
 Proof.
   induction s using sim_sequent_ind
     with (P0 := list_type_renaming) ;
-    intros ; subst ; econstructor ; eauto.
-  - assert (rw : up (ren ξ) = ren (upren ξ)). { autosubst. }
-    rewrite rw. apply IHs. autosubst.
+    intros ; subst ; asimpl ; econstructor ; eauto.
+  - apply IHs. autosubst.
+  - apply IHs1. autosubst.
 Qed.      
 
 Definition list_type_substitution Γ A l B (ls:list_sequent Γ A l B) :=
@@ -59,9 +85,12 @@ Lemma type_substitution Γ t A (s: sequent Γ t A) :
 Proof.  
   induction s using sim_sequent_ind
     with (P0 := list_type_substitution) ;
-    intros ; subst ; try econstructor ; eauto.
-  - apply IHs. destruct x ; asimpl.
-    + constructor. trivial.
+    intros ; asimpl ; subst ; try econstructor ; eauto.
+  - apply IHs. destruct x ; asimpl ; auto.
+    + eapply type_renaming ; eauto.
+  - eapply app_is_admissible ; eauto.
+    rewrite<- e. apply H.
+  - apply IHs1. destruct x ; asimpl ; auto.
     + eapply type_renaming ; eauto.
 Qed.        
 
@@ -73,11 +102,10 @@ Lemma beta1_type_preservation :
 Proof.
   intros.
   inversion H ; subst.
-  inversion H0 ; subst. (* decompor: mApp (Lam t0) u [] *)
-  inversion H4 ; subst. (* decompor: Lam t0 *)
+  inversion H0 ; subst. (* decompor: LambApp t0 u [] *)
   inversion H7 ; subst. (* unificar: tipos A = B usando [] *)
   eapply type_substitution.
-  - eapply H3.
+  - eapply H4.
   - destruct x ; asimpl ; eauto.
 Qed.
 
@@ -86,40 +114,18 @@ Lemma beta2_type_preservation :
 Proof.
   intros.
   inversion H ; subst.
-  inversion H0 ; subst. (* decompor: mApp (Lam t0) u (v::l) *)
-  inversion H4 ; subst. (* decompor: Lam t0 *)
+  inversion H0 ; subst. (* decompor: LambApp t0 u (v::l) *)
   inversion H7 ; subst. (* decompor: v::l *)
-  inversion H10 ; subst. (* unificar: tipos A = B0 usando l *)
-  - econstructor ; eauto.
-    eapply type_substitution.
-    + eapply H3.
+  inversion H9 ; subst. (* unificar: tipos A = B0 usando l *)
+  - eapply app_is_admissible ; eauto.
+    eapply type_substitution ; eauto.
     + destruct x ; asimpl ; eauto.
-  - econstructor ; eauto.
-    eapply type_substitution.
-    + eapply H3.
+  - eapply app_is_admissible ; eauto.
+    eapply type_substitution ; eauto.
     + destruct x ; asimpl ; eauto.
 Qed.
-
-Lemma append_is_admissible Γ l l' A B C :
-  list_sequent Γ A l B -> list_sequent Γ B l' C ->
-  list_sequent Γ A (l++l') C.
-Proof.                  
-  intros H1 H2.
-  induction H1 ; asimpl ; auto.
-Qed.
   
-Lemma h_type_preservation :
-  forall t t', LambdaM.H t t' -> forall Γ A, sequent Γ t A -> sequent Γ t' A.
-Proof.
-  intros.
-  inversion H ; subst.
-  inversion H0 ; subst. (* decompor mApp (mApp t0 u l) l' *)
-  inversion H4 ; subst. (* decompor mApp t0 u l *)
-  econstructor ; eauto.
-  - eapply append_is_admissible ; eauto.
-Qed.    
-  
-Definition list_type_preservation (l l': list λm) (_: step' l l') :=
+Definition list_type_preservation (l l': list λc) (_: step' l l') :=
   forall Γ A B, list_sequent Γ A l B -> list_sequent Γ A l' B.
 
 Hint Unfold list_type_preservation : core.
@@ -134,10 +140,8 @@ Proof.
     try (now inversion H0 ; econstructor ; eauto).
     
   - inversion b.
-    + inversion H0.
-      * eapply beta1_type_preservation ; eassumption.
-      * eapply beta2_type_preservation ; eassumption.
-    + eapply h_type_preservation ; eassumption.
+    + eapply beta1_type_preservation ; eassumption.
+    + eapply beta2_type_preservation ; eassumption.
 Qed.
 
 Corollary type_preservation' Γ t A (_: sequent Γ t A) :
