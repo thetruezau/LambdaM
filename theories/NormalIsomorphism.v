15a8e7bbd81f7ac93c6ab3c9ff2953640abf5a6c
thetruezau <80555837+thetruezau@users.noreply.github.com>
2025-06-16 16:04:42 +0100

    Proof of the bijection between normal forms

---
+++ theories/NormalIsomorphism.v
@@ -0,0 +1,38 @@
+Require Import List.
+Require Import Autosubst.Autosubst.
+Require Import Coq.Relations.Relation_Definitions.
+
+Require Import Lambda Canonical LambdaIsomorphism.
+
+Import ListNotations.
+
+Theorem inversion1 :
+  (forall t, Canonical.normal t -> ψ (θ t) = t)
+  /\
+  (forall l, Canonical.normal_list l -> forall s, ψ (θ' s l) = ψ' s l).
+Proof.
+  apply Canonical.mut_normal_ind ; intros ; asimpl ; try easy.
+  - now f_equal. 
+  - fold (θ' (App (Var x) (θ u)) l).
+    fold (ψ (θ' (App (Var x) (θ u)) l)).
+    rewrite H0. asimpl. now f_equal.
+  - fold (θ' (App s (θ u)) l).
+    fold (ψ (θ' (App s (θ u)) l)).
+    rewrite H0. asimpl. now repeat f_equal.
+Qed.
+
+Theorem inversion2 :
+  (forall s, Lambda.normal s -> θ (ψ s) = s)
+  /\
+  (forall a, Lambda.apps a -> forall l, θ (ψ' a l) = θ' a l).
+Proof.
+  apply Lambda.mut_normal_ind ; intros.
+  - asimpl. now f_equal.
+  - now apply H with (l:=[]).
+  - destruct l ; now asimpl.
+  - asimpl. fold (ψ s2).
+    rewrite H. asimpl.
+    fold (θ' (App s1 (θ (ψ s2))) l).
+    now rewrite H0.
+Qed.
+
